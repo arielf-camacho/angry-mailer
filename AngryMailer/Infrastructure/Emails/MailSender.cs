ef5d0b3502c5026e5631fa5b6a8eddb0163310a8
Ariel Felipe Camacho Diaz <arielf.camacho@gmail.com>
2021-12-17 20:35:27 -0300

    Add email-sending initial types
    - Include namespace for Domain classes.
    - Include namespace for Infrastructure classes.
    - Include the domain entity: Email.
    - Include contract of an email sending service.

---
+++ AngryMailer/Infrastructure/Emails/MailSender.cs
@@ -0,0 +1,17 @@
+﻿using AngryMailer.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace AngryMailer.Domain
+{
+    /// <summary>
+    ///     Default implementation of <see cref="IMailSender"/>. See that interface's docs for more details.
+    /// </summary>
+    public class MailSender : IMailSender
+    {
+        /// <inheritdoc cref="IMailSender.SendMail(Email)"/>
+        public Task SendMail(Email email)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
