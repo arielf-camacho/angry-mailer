4bc81f5a25159f53c8a8f350b040bc9380aaa874
Ariel Felipe Camacho Diaz <arielf.camacho@gmail.com>
2021-12-17 19:48:58 -0300

    Better layout project - Renamed main windows to SendMailView. - Include Microsoft.Practices.Unity For DI. - Moved views to Views namespace. - Created ViewModels namespace.

---
+++ AngryMailer/Views/SendMailView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace AngryMailer.Views
+{
+    /// <summary>
+    /// Interaction logic for SendMailView.xaml
+    /// </summary>
+    public partial class SendMailView : Window
+    {
+        public SendMailView()
+        {
+            InitializeComponent();
+        }
+    }
+}
