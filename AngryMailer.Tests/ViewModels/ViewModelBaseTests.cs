a26d080dc501b553ec03224fac9eb15b4c2ab5de
Ariel Felipe Camacho Diaz <arielf.camacho@gmail.com>
2021-12-17 20:21:15 -0300

    Test ViewModelBase class
    - Added tests project.
    - Unit tested the ViewModelBase class.
    - Made visible the internal types from the AngryMailer project to its tests project.

---
+++ AngryMailer.Tests/ViewModels/ViewModelBaseTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AngryMailer.ViewModels;
+
+namespace AngryMailer.Tests.ViewModels
+{
+    [TestClass]
+    public class ViewModelBaseTests
+    {
+        [TestMethod]
+        public void NotifyPropertyChanged_WhenInvoked_ItNotifiesTheNotifyingPropertyHasChanged()
+        {
+            // Given
+            bool notificationArrived = false;
+
+            var subject = new ViewModel();
+            subject.PropertyChanged += (_, args) => notificationArrived = args.PropertyName == nameof(ViewModel.Name);
+
+            // When
+            subject.Name = "Some value";
+
+            // Then
+            Assert.IsTrue(notificationArrived);
+        }
+
+
+        private class ViewModel : ViewModelBase
+        {
+            private string? _name;
+
+            public string? Name  
+            {
+                get => _name;
+                set
+                {
+                    _name = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+    }
+}
---
+++ AngryMailer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AngryMailer.ViewModels
+{
+    /// <summary>
+    ///     Base class of all the view models in the current project. By view model we reffer to the role with the 
+    ///     same name in the MVVM pattern.
+    /// </summary>
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        ///     Event that notifies when a property in this view model has changed.
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        /// <summary>
+        ///     When invoked it notifies to <see cref="PropertyChanged"/> event registered listeners that the 
+        ///     property calling this method has changed.
+        ///     
+        ///     NOTE: Please use this method only inside property setters.
+        /// </summary>
+        /// <param name="propertyName">Name of the changed property's setter method.</param>
+        protected void NotifyPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            var property = propertyName?[4..];
+            var handlers = PropertyChanged;
+
+            handlers?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
