d64a7bbe6c8b905b0d8859b96e4dae2ac5cb08e4
Ariel Felipe Camacho Diaz <arielf.camacho@gmail.com>
2021-12-18 03:12:57 -0300

    Add WPF validation to SendMailViewModel
    - It now implements IDataErrorInfo and is using internally the Email domain entity validation logic.
    - SendMailViewModel email data is initialized to empty strings by default.
    - Remove inefficient Email domain entity building method: BuildEmail().
    - Add the DataBinding validations to the SendMailView's view model.

--- AngryMailer/ViewModels/SendMailViewModel.cs
+++ AngryMailer/ViewModels/SendMailViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using AngryMailer.Domain;
 using AngryMailer.Domain.Entities;
 using System;
+using System.ComponentModel;
 using System.Windows.Input;
 
 namespace AngryMailer.ViewModels
@@ -8,11 +9,12 @@ namespace AngryMailer.ViewModels
     /// <summary>
     ///     Represents the view models.
     /// </summary>
-    public class SendMailViewModel : ViewModelBase
+    public class SendMailViewModel : ViewModelBase, IDataErrorInfo
     {
         private string _to = string.Empty;
         private string _subject = string.Empty;
         private string _content = string.Empty;
+        private Email _email;
 
         private IMailService _mailSender;
 
@@ -26,6 +28,8 @@ namespace AngryMailer.ViewModels
         public SendMailViewModel(IMailService mailSender)
         {
             _mailSender = mailSender;
+            _email = new Email(_to, _subject, _content);
+
             SendEmailCommand = new Command(ExecuteSendMailCommand, CanSendEmailCommand);
         }
 
@@ -46,6 +50,7 @@ namespace AngryMailer.ViewModels
                 if (_to == value) return;
 
                 _to = value;
+                _email = _email with { ToAddress = _to };
                 NotifyPropertyChanged();
             }
         }
@@ -61,6 +66,7 @@ namespace AngryMailer.ViewModels
                 if (_subject == value) return;
 
                 _subject = value;
+                _email = _email with { Subject = _subject };
                 NotifyPropertyChanged();
             }
         }
@@ -76,25 +82,28 @@ namespace AngryMailer.ViewModels
                 if (_content == value) return;
 
                 _content = value;
+                _email = _email with { Content = _content };
                 NotifyPropertyChanged();
             }
         }
 
+        /// <summary>
+        ///     Gets an error that describes overall what can be wrong with the email data.
+        /// </summary>
+        public string Error => _email.IsValid ? string.Empty : "Email data is invalid";
 
-        private bool CanSendEmailCommand(object? arg)
-        {
-            var email = BuildEmail();
+        /// <summary>
+        ///     Gets the validation error that can exist for the specified property.
+        /// </summary>
+        /// <param name="columnName">Name of the field to validate.</param>
+        /// <returns>
+        ///     A string representing the validation error that can exist in the property name as 
+        ///     <paramref name="columnName"/>; or empty string if property is valid.
+        /// </returns>
+        public string this[string columnName] => _email.Validate(columnName) ?? string.Empty;
 
-            return email.IsValid;
-        }
+        private bool CanSendEmailCommand(object? arg) => _email.IsValid;
 
-        private void ExecuteSendMailCommand(object? obj)
-        {
-            var email = BuildEmail();
-
-            _mailSender.Send(email);
-        }
-
-        private Email BuildEmail() => new(ToAddress, Subject, Content);
+        private void ExecuteSendMailCommand(object? obj) => _mailSender.Send(_email);
     }
 }
