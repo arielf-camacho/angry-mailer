d65815e050c169eb130c7ced324e310e6deb503a
Ariel Felipe Camacho Diaz <arielf.camacho@gmail.com>
2021-12-18 15:05:19 -0300

    Add a MessageService
    - Allowing to show dialogs with a certain message to the User.

---
+++ AngryMailer/Infrastructure/MessageService.cs
@@ -0,0 +1,17 @@
+﻿using AngryMailer.Domain.Services;
+using System.Windows;
+
+namespace AngryMailer.Infrastructure
+{
+    /// <summary>
+    ///     Default implementation of the <see cref="IMessageService"/> interface. See that interface's docs for more details.
+    /// </summary>
+    public class MessageService : IMessageService
+    {
+        /// <inheritdoc cref="IMessageService.ShowMessage(string)"/>
+        public void ShowMessage(string message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+}
