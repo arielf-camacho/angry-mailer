b0a65b002a72ef3100ef6308d1a94d61c2b4565b
Ariel Felipe Camacho Diaz <arielf.camacho@gmail.com>
2021-12-18 05:05:41 -0300

    Command now is a base contract
    - Command renamed to CommandBase and made abstract.
    - It only now knows that it can execute always.
    - And contains as abstract the Execute method from the ICommand contract.

---
+++ AngryMailer.Tests/ViewModels/CommandBaseTests.cs
@@ -0,0 +1,31 @@
+﻿using AngryMailer.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AngryMailer.Tests.ViewModels
+{
+    [TestClass]
+    public class CommandBaseTests
+    {
+        [TestMethod]
+        public void CanExecute_AlwaysReturnsTrue()
+        {
+            // Given
+            var subject = new CommandStub();
+
+            // When
+            var canExecute = subject.CanExecute(null);
+
+            // Then
+            Assert.IsTrue(canExecute);
+        }
+
+        
+        private class CommandStub : CommandBase
+        {
+            public override void Execute(object? parameter)
+            {
+            }
+        }
+    }
+}
--- AngryMailer/ViewModels/CommandBase.cs
+++ AngryMailer/ViewModels/CommandBase.cs
@@ -6,27 +6,8 @@ namespace AngryMailer.ViewModels
     /// <summary>
     ///     Defines the default implementation of a command as view models should provide them.
     /// </summary>
-    public class Command : ICommand
+    public abstract class CommandBase : ICommand
     {
-        private readonly Action<object?> _execute;
-        private readonly Func<object?, bool> _canExecute;
-
-
-        /// <summary>
-        ///     Initializes a new instance of <see cref="Command"/> given a "execute" and (optionally) an 
-        ///     "can execute" method.
-        /// </summary>
-        /// <param name="execute">Method to invoke when the command instance gets executed.</param>
-        /// <param name="canExecute">
-        ///     Method to invoke to determine whether the command instance can or cannot be executed.
-        /// </param>
-        public Command(Action<object?> execute, Func<object?, bool>? canExecute = null)
-        {
-            _execute = execute;
-            _canExecute = canExecute ?? (o => true);
-        }
-
-
         /// <summary>
         ///     Event that is raised by the <see cref="CommandManager"/> to notify the bound WPF controls
         ///     to check about the availability of the Command, so they can refresh their enabled state.
@@ -48,14 +29,15 @@ namespace AngryMailer.ViewModels
         /// <returns>
         ///     True if the command can be executed with the given <paramref name="parameter"/>; false otherwise.
         /// </returns>
-        public bool CanExecute(object? parameter) => _canExecute(parameter);
+        public virtual bool CanExecute(object? parameter) => true;
 
         /// <summary>
-        ///     Invokes the logic of this command given a (nullable) parameter.
+        ///     When overridden in a derived class it contains the actual logic of this command given a (nullable)
+        ///     parameter.
         /// </summary>
         /// <param name="parameter">
         ///     The parameters to invoke the command with. Can be null.
         /// </param>
-        public void Execute(object? parameter) => _execute(parameter);
+        public abstract void Execute(object? parameter);
     }
 }
