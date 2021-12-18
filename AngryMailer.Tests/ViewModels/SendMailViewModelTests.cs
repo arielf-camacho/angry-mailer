65b2c909a4bdb4f0650a59310dc4236e4276a77c
Ariel Felipe Camacho Diaz <arielf.camacho@gmail.com>
2021-12-17 23:53:48 -0300

    Implement email sending logic
    - Rename Email's Body property to Content.
    - Rename Email's To property to ToEmail.
    - Turn IMailSender.Send method synchronous.
    - Implement MailService.Send method to show the ToString() version of given Email (simulating it was sent).
    - Implement/test the SendCommand's execute method of the SendMailViewModel.

--- AngryMailer.Tests/ViewModels/SendMailViewModelTests.cs
+++ AngryMailer.Tests/ViewModels/SendMailViewModelTests.cs
@@ -1,8 +1,12 @@
-﻿using AngryMailer.Tests.Utils;
+﻿using AngryMailer.Domain;
+using AngryMailer.Tests.Utils;
 using AngryMailer.ViewModels;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.ComponentModel;
+using NSubstitute;
+using System.Threading.Tasks;
+using AngryMailer.Domain.Entities;
+using System.Text;
 
 namespace AngryMailer.Tests.ViewModels
 {
@@ -11,13 +15,17 @@ namespace AngryMailer.Tests.ViewModels
     {
         private static readonly Random _random = new(Environment.TickCount);
 
+        private IMailService _mailService;
+
         private SendMailViewModel? _subject;
 
 
         [TestInitialize]
         public void Initialize()
         {
-            _subject = new SendMailViewModel();
+            _mailService = Substitute.For<IMailService>();
+
+            _subject = new SendMailViewModel(_mailService);
         }
 
 
@@ -44,6 +52,31 @@ namespace AngryMailer.Tests.ViewModels
 
             // Then
             Assert.IsTrue(canExecute);
+        }
+
+        [TestMethod]
+        public void SendCommand_Execute_SendsEmailWithViewModelData()
+        {
+            const string email = "john@mail.com";
+            const string emailSubject = "Happy birthday";
+            var contentBuilder = new StringBuilder("Hello John:");
+            contentBuilder.AppendLine("Hope you are ok, my friend. I wanted to wish you happy birthday.");
+            contentBuilder.AppendLine("Have the best day in the world.");
+            contentBuilder.AppendLine("Yours,");
+            contentBuilder.AppendLine("James");
+            var content = contentBuilder.ToString();
+
+            _subject!.ToEmail = email;
+            _subject!.Subject = emailSubject;
+            _subject!.Content = content;
+
+            // When
+            _subject!.SendCommand.Execute(null);
+
+            // Then
+            _mailService
+                .Received()
+                .Send(Arg.Is<Email>(email => email.Content == content && email.ToEmail == email.ToEmail && email.Subject == emailSubject));
         }
 
         [TestMethod]
